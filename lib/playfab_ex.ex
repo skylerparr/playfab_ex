be28420b3efb08d81b05476c6889f6f7fff27bfb
Skyler Parr <skylerparr@gmail.com>
2018-01-27 14:08:16 -0800

    initial commit

---
+++ lib/playfab_ex.ex
@@ -0,0 +1,18 @@
+defmodule PlayfabEx do
+  @moduledoc """
+  Documentation for PlayfabEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlayfabEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/playfab_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule PlayfabExTest do
+  use ExUnit.Case
+  doctest PlayfabEx
+
+  test "greets the world" do
+    assert PlayfabEx.hello() == :world
+  end
+end
