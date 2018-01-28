b6a049511bc3466cea5959797425bffc099239de
Skyler Parr <skylerparr@gmail.com>
2018-01-28 08:39:28 -0800

    bumped version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlayfabEx.Mixfile do
   def project do
     [
       app: :playfab_ex,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: description(),
